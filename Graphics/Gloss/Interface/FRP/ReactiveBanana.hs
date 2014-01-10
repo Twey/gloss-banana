3b502f09efad3f925d704b165ab68fb19f1af55c
James ‘Twey’ Kay <twey@twey.co.uk>
2014-01-10 20:43:33 +0000

    Fix Haddock

--- Graphics/Gloss/Interface/FRP/ReactiveBanana.hs
+++ Graphics/Gloss/Interface/FRP/ReactiveBanana.hs
@@ -7,7 +7,7 @@ import Reactive.Banana
 import Reactive.Banana.Frameworks
 import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 
--- | A useful type synonym to avoid confusion between
+-- | A useful type synonym for Gloss event values, to avoid confusion between
 --   Gloss and ReactiveBanana.
 type InputEvent = G.Event
 
@@ -17,16 +17,19 @@ playBanana ∷ Display -- ^ The display method
            → Color   -- ^ The background colour
            → Int     -- ^ The refresh rate, in Hertz
            → (∀ t. Frameworks t
-              ⇒ Event t Float      -- ^ A stream of refresh events, with
-                                   --   time delta since last refresh
-              → Event t InputEvent -- ^ A stream of input events
+              ⇒ Event t Float
+              → Event t InputEvent
               → Moment t (Behavior t Picture))
+           -- ^ A Moment t action to generate the Picture Behavior, taking
+           --   the refresh and input Events with respect to which to build it.
+           --   The refresh event generates a Float indicating the time delta
+           --   since the last refresh.
            → IO ()
 playBanana display colour frequency mPicture = do
   pictureref ← newIORef blank
   (tickHandler,  tick)  ← newAddHandler
   (eventHandler, event) ← newAddHandler
-  compile (makeNetwork tickHandler eventHandler (change pictureref)) >>= actuate
+  compile (makeNetwork tickHandler eventHandler $ change pictureref) >>= actuate
   playIO display colour frequency ()
     (\      _ → readIORef pictureref)
     (\ ev   _ → () <$ event ev)
